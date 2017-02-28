3c9f28cd7cd87a6564856d17f8d52461a597f7ca
user <user@userui-mac-pro.local>
2017-02-28 18:08:06 +0900

    Initial Commit

---
+++ meankg/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  meankg
+//
+//  Created by user on 2017. 2. 28..
+//  Copyright © 2017년 meankg. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
